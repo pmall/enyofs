7ea6a857de6e4dc100449037a257cd8778201d33
pierre <pm@enyopharma.com>
2015-10-22 15:42:41 +0200

    Upload ok :)

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -24,8 +24,7 @@ class FilesController extends Controller
 
     public function show ($filename)
     {
-        // Quick fix
-        $filename = '/' . $filename;
+        $filename = '/' . trim($filename, ' /');
 
         $file = $this->gridfs->findOne($filename);
 
@@ -43,8 +42,32 @@ class FilesController extends Controller
         }
     }
 
-    public function store (Request $request, $filename)
+    public function store ($filename, Request $request)
     {
+        // Check if sent data are ok
+        if (! $request->hasFile('file')) return abort(500, 'No file sent.');
+        if (! $request->file('file')->isValid()) return abort(500, 'Upload error.');
+
+        // Format filename
+        $filename = '/' . trim($filename, ' /');
+
+        // Check if filename already exists
+        $existing = $this->gridfs->findOne($filename);
+
+        if ($existing) abort(403, "File already exists.");
+
+        // Determine the mime type (content type of the request or mime type of
+        // the file if not specified)
+        $mime = $request->get('mime', $request->file('file')->getMimeType());
+
+        // Store the file in gridfs
+        $this->gridfs->storeUpload('file', [
+            'filename' => $filename,
+            'mime' => $mime,
+            'date' => new \MongoDate(),
+        ]);
+
+        // Return the filename as success
         return $filename;
     }
 }
